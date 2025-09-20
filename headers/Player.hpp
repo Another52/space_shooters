f29d2cc0ef35a7ef35f8722473394d8f95cb52c8
Another52 <syrinxofficial446@gmail.com>
2025-09-20 16:10:55 +0530

    upgrade damage of player

--- headers/Player.hpp
+++ headers/Player.hpp
@@ -8,5 +8,5 @@ public:
 	Player(const sf::RenderWindow& window, TextureManager& texManager, const std::string& filename, const sf::IntRect& rect);
 	void Update(float dt) override;
 private:
-	int damage{ 1 };
+	int damage{ 5 };
 };
