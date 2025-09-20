799f446190c1a4636f4e396292e76b0f5ead030f
Another52 <syrinxofficial446@gmail.com>
2025-09-20 21:37:37 +0530

    background updated to draw 9 tiles

--- headers/Background.hpp
+++ headers/Background.hpp
@@ -2,17 +2,18 @@
 
 #include <SFML/Graphics.hpp>
 #include "TextureManager.hpp"
-#include "Camera.hpp"
 #include <vector>
 #include <string>
 
 class Background
 {
 public:
-	Background(TextureManager& texManager, const std::string& filename);
-	void Update(const Camera& camera);
-	void Draw(sf::RenderWindow& window) const;
+	Background(TextureManager& texManager, const sf::Sprite& player, const std::string& filename);
+	void Update(const sf::View& view);
+	void Draw(sf::RenderWindow& window);
 private:
+	bool done = false;
 	const sf::Texture& texture;
+	const sf::Sprite& player;
 	std::vector<sf::Sprite> sprites;
 };
--- src/Background.cpp
+++ src/Background.cpp
@@ -1,21 +1,50 @@
 #include "Background.hpp"
+#include <iostream>
 
-Background::Background(TextureManager& texManager, const std::string& filename)
+Background::Background(TextureManager& texManager,
+					   const sf::Sprite& player, const std::string& filename)
 	:
-	texture(texManager.GetTexture(filename))
+	texture(texManager.GetTexture(filename)),
+	player(player)
 {
-	sf::Sprite sprite(texture);
-	sprite.scale({ 0.9f, 0.9f });
-	sprite.setOrigin(sprite.getLocalBounds().getCenter());
-	sprites.push_back(sprite);
+	sprites.reserve(9);
+
+	constexpr float scaleFactor = 1.5f;
+	constexpr float offset = 1.0f;
+	constexpr sf::Vector2f scale(scaleFactor, scaleFactor);
+	const float spriteWidth = texture.getSize().x * scale.x;
+	const sf::Vector2f center = player.getPosition();
+	int num = 1;
+
+	for (int i = -num; i <= num; ++i)
+	{
+		for (int j = -num; j <= num; j++)
+		{
+			sf::Sprite sprite(texture);
+			sprite.setScale(scale);
+			sprite.setOrigin(sprite.getLocalBounds().getCenter());
+			sprite.setPosition({ center.x + i * (spriteWidth * offset), center.y + j * (spriteWidth * offset) });
+			sprites.push_back(sprite);
+		}
+	}
 }
 
-void Background::Update(const Camera& camera)
+void Background::Update(const sf::View& view)
 {
+	const sf::Vector2f center = view.getCenter();
+	const float spriteWidth = texture.getSize().x;
 
+	for (int y = -1; y <= 1; ++y)
+	{
+		for (int x = -1; x <= 1; ++x)
+		{
+			int index = (y + 2) * (x + 1);
+			auto& sprite = sprites.at(index);
+		}
+	}
 }
 
-void Background::Draw(sf::RenderWindow& window) const
+void Background::Draw(sf::RenderWindow& window)
 {
 	for (auto& sprite : sprites)
 	{
