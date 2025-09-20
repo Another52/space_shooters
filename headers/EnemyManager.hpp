de2d64d4bf3b3ecae11bfbdbe89a14b0fc07aff2
Another52 <syrinxofficial446@gmail.com>
2025-09-20 15:39:27 +0530

    enemy manager added

---
+++ headers/EnemyManager.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Enemy.hpp"
+#include <vector>
+#include <memory>
+
+class EnemyManager
+{
+public:
+	EnemyManager(const sf::Sprite& player, const sf::RenderWindow& window,
+				 TextureManager& texManager, const std::string& filename, sf::IntRect& rectangle);
+	void Update(float deltatime);
+	void Draw(sf::RenderWindow& window);
+	void Collide();
+	void Spawn();
+	const std::vector<std::unique_ptr<Enemy>>& GetEnemies() { return enemies; }
+private:
+	std::vector<std::unique_ptr<Enemy>> enemies;
+	const sf::Sprite& player;
+	const sf::RenderWindow& window;
+	TextureManager& texManager;
+	const std::string& filename;
+	sf::IntRect& rectangle;
+	float spawnTimer{ 0.f };
+	float spawnInterval{ 2.f };
+};
---
+++ src/EnemyManager.cpp
@@ -0,0 +1,49 @@
+#include "EnemyManager.hpp"
+
+EnemyManager::EnemyManager(const sf::Sprite& player, const sf::RenderWindow& window, TextureManager& texManager,
+						   const std::string& filename, sf::IntRect& rectangle)
+	:
+	player(player), window(window), texManager(texManager),
+	filename(filename), rectangle(rectangle)
+{
+}
+
+void EnemyManager::Update(float deltatime)
+{
+	spawnTimer += deltatime;
+	if (spawnTimer > spawnInterval)
+	{
+		Spawn();
+	}
+
+	for (auto& enemy : enemies)
+	{
+		enemy->Update(deltatime);
+	}
+
+	enemies.erase(
+		std::remove_if(enemies.begin(), enemies.end(),
+					   [](const std::unique_ptr<Enemy>& e) { return e->IsDead(); }),
+		enemies.end());
+}
+
+void EnemyManager::Draw(sf::RenderWindow& window)
+{
+	for (auto& enemy : enemies)
+	{
+		enemy->Draw(window);
+	}
+}
+
+void EnemyManager::Collide()
+{
+	for (auto& enemy : enemies)
+	{
+		//enemy->Update();
+	}
+}
+
+void EnemyManager::Spawn()
+{
+	enemies.push_back(std::make_unique<Enemy>(player, window, texManager, filename, rectangle));
+}
